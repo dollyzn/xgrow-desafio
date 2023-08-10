a18144527d0a023ff77513ec9c2fd42efc859b7a
Natã <natasjesus1@gmail.com>
2023-08-09 21:54:32 -0300

    feat(user): implement create

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const data: Prisma.UserCreateInput = {
+      ...createUserDto,
+    };
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const userExists = await this.prisma.user.findFirst({
+      where: { email: data.email },
+    });
+
+    if (userExists) {
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    }
+
+    const createdUser = await this.prisma.user.create({ data });
+
+    return {
+      ...createdUser,
+    };
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
